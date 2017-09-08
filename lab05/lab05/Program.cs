8a0ad6cad4b2e73440814a3222fc73dbe2230dbc
Jirapat17412 <tangmo17412@hotmail.com>
2017-09-08 13:03:28 +0700

    ED by 1.1.3(exception)-Null

--- lab05/lab05/Program.cs
+++ lab05/lab05/Program.cs
@@ -32,7 +32,7 @@ namespace lab05
         }
         private static int div(int a, int b)
         {
-            throw new DivideByZeroException();
+            throw new NullReferenceException();
         }
     }
 }
