b1900398104c129083e6494a220ae3a3c0e5547e
Jirapat17412 <tangmo17412@hotmail.com>
2017-09-08 13:36:09 +0700

    ED by for

--- lab05/lab05/Program.cs
+++ lab05/lab05/Program.cs
@@ -5,24 +5,10 @@ namespace lab05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("---- Begin of While statement.");
-            int n = 1;
-            while (n < 1)
+            for (int i = 1; i <= 5; i++)
             {
-                Console.WriteLine("  Current value of n is {0}", n);
-                n++;
+                Console.WriteLine(i);
             }
-            Console.WriteLine("---- End of While statement.");
-
-            Console.WriteLine("---- Begin of Do..While statement.");
-            n = 1;
-            do
-            {
-                Console.WriteLine("  Current value of n is {0}", n);
-                n++;
-            }
-            while (n < 1);
-            Console.WriteLine("---- End of Do..While statement.");
         }
     }
 }
